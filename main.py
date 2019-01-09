6daf0d07afe8a3416ee7346408bad3970514dc73
jaegern <niklas.jaeger97@gmail.com>
2019-01-09 13:15:46 +0100

    blabla

--- main.py
+++ main.py
@@ -6,7 +6,13 @@ import os.path
 from pyds import MassFunction
 from enum import Enum
 from pathlib import Path
-
+class Emotion(Enum):
+     FEAR =0
+     SURPRISE= 1
+     ANGER = 2
+     JOY = 3
+     DISGUST = 4
+     SADNESS = 5
 def main():
     "main function"
 
@@ -34,10 +40,38 @@ def main():
                     "sound_intensity" : row[3]
                     })
             for el_tact in tacts:
+                omega = {"su", 'f', 'j', 'a', 'sa', 'd'}
                 print(el_tact)
-            omega = {"su", 'f', 'j', 'a', 'sa', 'd'}
-            m1 = MassFunction([({"sa", 'd'}, 0.8), (omega, 0.2)])
-            print(m1.bel("sa"))
+
+                if float(el_tact["speaking_rate"].replace(",",".")) < 4:
+                    m1 = MassFunction([({'sa', 'd'}, 0.8), (omega, 0.2)])
+                elif(float(el_tact["speaking_rate"].replace(",","."))>= 4 and float(el_tact["speaking_rate"].replace(",",".")) < 5):
+                    m1= MassFunction([(omega, 1)])
+                elif float(el_tact["speaking_rate"].replace(",",".")) >= 5:
+                    m1= MassFunction([({'j', 'f', 's', 'a'}, 0.8), (omega, 0.2)])
+                else:
+                    m1 = None
+
+                if el_tact["avg_pitch"] == "niedrig" or el_tact["avg_pitch"] == "sehr niedrig" :
+                    m2 = MassFunction([({'d','sa'},0.8),(omega,0.2)])
+                elif el_tact["avg_pitch"] == "normal":
+                    m2 = MassFunction([(omega,1)])
+                elif el_tact["avg_pitch"] == "hoch" or el_tact["avg_pitch"] == "sehr hoch":
+                    m2 = MassFunction([({'j','f','su','a'}, 0.8), (omega, 0.2)])
+                else:
+                    m2 = None
+
+                if el_tact["sound_intensity"] == "niedrig" or el_tact["sound_intensity"] == "sehr niedrig" :
+                    m3 = MassFunction([({'d','sa'},0.8),(omega,0.2)])
+                elif el_tact["sound_intensity"] == "normal":
+                    m3 = MassFunction([({'f'}, 0.6), (omega, 0.4)])
+                elif el_tact["sound_intensity"] == "hoch" or el_tact["sound_intensity"] == "sehr hoch":
+                    m3 = MassFunction([({'j','su','a'}, 0.8), (omega, 0.2)])
+                else:
+                    m3 = None
+
+                m4 = m1.combine_conjunctive([m2, m3], normalization=True)
+                print(m4.bel())
     else:
         print("Given path is invalid")
 
