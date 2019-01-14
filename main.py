76422d726a8484dea2f90f71f163aa562980706a
Moser <arian.moser@hpe.com>
2019-01-14 16:45:40 +0100

    Updated main

--- main.py
+++ main.py
@@ -60,39 +60,49 @@ def main():
             for el_tact in tacts:
                 omega = {"su", 'f', 'j', 'a', 'sa', 'd'}
                 print(el_tact)
-
-                if float(el_tact["speaking_rate"].replace(",", ".")) < 4:
-                    m_1 = MassFunction([({'d', 'sa'}, 0.8), (omega, 0.2)])
+                if float(el_tact["speaking_rate"].replace(",", ".")) < 3:
+                    m_1 = MassFunction([({'d'}, 0.5),({'j'}, 0.4), (omega, 0.1)])
+                elif float(el_tact["speaking_rate"].replace(",", ".")) < 4:
+                    m_1 = MassFunction([({'d'}, 0.4),({'j'}, 0.35), (omega, 0.25)])
                 elif(float(el_tact["speaking_rate"].replace(",", ".")) >= 4 and
                      float(el_tact["speaking_rate"].replace(",", ".")) < 5):
                     m_1 = MassFunction([(omega, 1)])
-                elif float(el_tact["speaking_rate"].replace(",", ".")) >= 5:
-                    m_1 = MassFunction([({'j', 'f', 's', 'a'}, 0.8), (omega, 0.2)])
+                elif (float(el_tact["speaking_rate"].replace(",", ".")) >= 5 and
+                    float(el_tact["speaking_rate"].replace(",", ".")) < 6):
+                    m_1 = MassFunction([({'j', 'f', 'su', 'a'}, 0.8), (omega, 0.2)])
+                elif float(el_tact["speaking_rate"].replace(",", ".")) >= 6:
+                    m_1 = MassFunction([({'j', 'f', 'su', 'a'}, 0.9), (omega, 0.1)])
                 else:
                     m_1 = None
 
-                if el_tact["avg_pitch"] == "niedrig" or el_tact["avg_pitch"] == "sehr niedrig":
-                    m_2 = MassFunction([({'d', 'sa'}, 0.8), (omega, 0.2)])
+                if el_tact["avg_pitch"] == "sehr niedrig":
+                    m_2 = MassFunction([({'d'}, 0.5),({'sa'}, 0.35), (omega, 0.15)])
+                elif el_tact["avg_pitch"] == "niedrig" :
+                    m_2 = MassFunction([({'d'}, 0.45), ({'sa'}, 0.3), (omega, 0.25)])
                 elif el_tact["avg_pitch"] == "normal":
                     m_2 = MassFunction([(omega, 1)])
-                elif el_tact["avg_pitch"] == "hoch" or el_tact["avg_pitch"] == "sehr hoch":
+                elif el_tact["avg_pitch"] == "hoch" :
                     m_2 = MassFunction([({'j', 'f', 'su', 'a'}, 0.8), (omega, 0.2)])
+                elif el_tact["avg_pitch"] == "sehr hoch":
+                    m_2 = MassFunction([({'j', 'f', 'su', 'a'}, 0.9), (omega, 0.1)])
                 else:
                     m_2 = None
-
-                if el_tact["sound_intensity"] == "niedrig" or el_tact["sound_intensity"] == "sehr niedrig":
+                if  el_tact["sound_intensity"] == "sehr niedrig":
+                    m_3 = MassFunction([({'d', 'sa'}, 0.9), (omega, 0.1)])
+                elif el_tact["sound_intensity"] == "niedrig" :
                     m_3 = MassFunction([({'d', 'sa'}, 0.8), (omega, 0.2)])
                 elif el_tact["sound_intensity"] == "normal":
-                    m_3 = MassFunction([({'f'}, 0.6), (omega, 0.4)])
-                elif el_tact["sound_intensity"] == "hoch" or el_tact["sound_intensity"] == "sehr hoch":
+                    m_3 = MassFunction([(omega, 1)])
+                elif el_tact["sound_intensity"] == "hoch":
                     m_3 = MassFunction([({'j', 'su', 'a'}, 0.8), (omega, 0.2)])
+                elif el_tact["sound_intensity"] == "sehr hoch":
+                    m_3 = MassFunction([({'j', 'su', 'a'}, 0.9), (omega, 0.1)])
                 else:
                     m_3 = None
-                m_4 = m_1.combine_conjunctive([m_2, m_3], normalization=True)
+                m_4 = m_1.combine_conjunctive([m_2,m_3],normalization=True)
                 emotion = str(m_4.max_pl())[12:-3]
                 print(m_4)
-                print("Die plausibelste Emotion ist " + emotionToString(emotion) + ": " + str(m_4.pl({emotion})))
-
+                print("Die plausibelste Emotion ist " + emotionToString(emotion)+": " + str(round(m_4.pl({emotion}), 3)))
     else:
         print("Given path is invalid")
 
